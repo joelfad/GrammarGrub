3c598e8d67cceb226b2c301c70d5564aa7a9caf2
Joel McFadden <dev@joelmcfadden.com>
2023-04-27 22:35:34 +0400

    install/configure eslint (airbnb) for typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,26 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
-    'eslint:recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react-refresh',
+  ],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 0,
   },
-}
+};
